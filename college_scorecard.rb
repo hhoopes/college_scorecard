0cc94a80aa80285cf2ecb3475fd90954d2d6c3df
hhoopes <heidih@gmail.com>
2016-02-04 13:02:13 -0700

    Pull out of a class, fix headers

--- college_scorecard.rb
+++ college_scorecard.rb
@@ -1,33 +1,19 @@
 require 'csv'
 require 'pry'
 
-class CollegeScorecard
-  attr_reader :reader, :file, :argument
+  # attr_reader :reader, :file, :argument
 
-  def initialize
-    @file = read_file
-    @processed = process_file(ARGV.first)
-    @argument = ARGV[1]
-    @opt_arg = ARGV[2] if ARGV[2]
-  end
+file = CSV.open "./2013_college_scorecards.csv", headers: true, header_converters: :symbol
+argument = ARGV[1]
+opt_arg = ARGV[2] if ARGV[2]
 
-  def read_file
-    #CSV.open file, headers: true, header_converters: :symbol
-    CSV.open "2013_college_scorecards.csv", headers: true, header_converters: :symbol
-  end
+if ARGV.first == "by_state"
+  file.select do |row|
+    row[:stabbr] == argument
+  end.map{|row| row[:instnm]}
 
-  def process_file(method)
-    if method == "by_state"
-      file.select do |row|
-        row[:STABBR] == argument
-        row[:INSTNM]
-      end
-    elsif method == "top_average_faculty_salary"
+elsif ARGV.first == "top_average_faculty_salary"
 
-    elsif method == "median_debt_between"
-    end
-  end
+elsif ARGV.first == "median_debt_between"
 
 end
-
-CollegeScorecard.new
