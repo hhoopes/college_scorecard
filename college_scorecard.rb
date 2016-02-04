8f43c02a8de5ce182b688c1836926fd5fc5920f7
hhoopes <heidih@gmail.com>
2016-02-04 13:14:55 -0700

    Output by state

--- college_scorecard.rb
+++ college_scorecard.rb
@@ -1,13 +1,12 @@
 require 'csv'
 require 'pry'
 
-  # attr_reader :reader, :file, :argument
-
 file = CSV.open "./2013_college_scorecards.csv", headers: true, header_converters: :symbol
 argument = ARGV[1]
 opt_arg = ARGV[2] if ARGV[2]
 
 if ARGV.first == "by_state"
+  results =
   file.select do |row|
     row[:stabbr] == argument
   end.map{|row| row[:instnm]}
@@ -17,3 +16,4 @@ elsif ARGV.first == "top_average_faculty_salary"
 elsif ARGV.first == "median_debt_between"
 
 end
+puts results
